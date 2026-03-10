53b07db7708d8756c1afa9db39c579931e081458
Allan <allanhenriqq24@gmail.com>
2026-03-10 12:31:48 -0300

    atualizando

---
+++ main.js
@@ -0,0 +1,10 @@
+const btn1 = document.getElementById('1');
+const btn2 = document.getElementById('1');
+const btn3 = document.getElementById('1');
+const btn4 = document.getElementById('1');
+const btn5 = document.getElementById('1');
+const btn6 = document.getElementById('1');
+const btn7 = document.getElementById('1');
+const btn8 = document.getElementById('1');
+const btn9 = document.getElementById('1');
+
