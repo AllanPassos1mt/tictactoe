3c8a8879a1fc76fa7ad67da836eadc339206c009
Allan <allanhenriqq24@gmail.com>
2026-03-12 10:38:54 -0300

    adicionando a função ganhar

--- main.js
+++ main.js
@@ -29,3 +29,20 @@ function limpar(){
 
     jogador = "X";
 }
+
+function ganhar() {
+    const p1 = document.getElementById("1").innerHTML;
+    const p2 = document.getElementById("2").innerHTML;
+    const p3 = document.getElementById("3").innerHTML;
+    const p4 = document.getElementById("4").innerHTML;
+    const p5 = document.getElementById("5").innerHTML;
+    const p6 = document.getElementById("6").innerHTML;
+    const p7 = document.getElementById("7").innerHTML;
+    const p8 = document.getElementById("8").innerHTML;
+    const p9 = document.getElementById("9").innerHTML;
+
+    if (p1 !== "" && p1 === p2 && p2 === p3) {
+        alert("Jogador " + p1 + " ganhou !" );
+    }
+
+}
